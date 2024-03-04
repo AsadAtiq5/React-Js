386f633ea2c8762b822bef98fa16bb0c506e896f
Asad Atiq <124272323+asadatiq1@users.noreply.github.com>
2024-03-04 19:02:23 +0500

    Project 1 css done

--- FoodyZone/src/App.jsx
+++ FoodyZone/src/App.jsx
@@ -1,5 +1,13 @@
+import "./App.css";
+import Body from "./components/Body";
+import Navigation from "./components/Navigation";
 const App = () => {
-  return <div>Code here</div>;
+  return (
+    <div>
+      <Navigation />
+      <Body />
+    </div>
+  );
 };
 
 export default App;
