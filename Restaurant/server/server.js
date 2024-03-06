a27864355ba7aff5347fac85ec1640227a52001f
Asad Atiq <124272323+asadatiq1@users.noreply.github.com>
2024-03-06 16:46:31 +0500

    Backend Setup done

---
+++ Restaurant/server/server.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const PORT = process.env.PORT || 5001;
+
+// Serve static files from the public/images directory
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+// Route to serve data in the specified format
+app.get("/api/menu", (req, res) => {
+  const menu = [
+    {
+      name: "Boiled Egg",
+      price: 10,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+      image: "/images/egg.png",
+      type: "breakfast",
+    },
+    {
+      name: "RAMEN",
+      price: 25,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+      image: "/images/ramen.png",
+      type: "lunch",
+    },
+    {
+      name: "GRILLED CHICKEN",
+      price: 45,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+      image: "/images/chicken.png",
+      type: "dinner",
+    },
+    {
+      name: "CAKE",
+      price: 18,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+      image: "/images/cake.png",
+      type: "breakfast",
+    },
+    {
+      name: "BURGER",
+      price: 23,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+      image: "/images/burger.png",
+      type: "lunch",
+    },
+    {
+      name: "PANCAKE",
+      price: 25,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+      image: "/images/pancake.png",
+      type: "dinner",
+    },
+  ];
+  res.json(menu);
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
